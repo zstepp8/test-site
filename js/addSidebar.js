f7b10e8e3801c40854c759d200901973cb528601
zstepp8 <zsteppbusiness@gmail.com>
2026-02-17 02:31:01 -0500

    Added website

---
+++ js/addSidebar.js
@@ -0,0 +1,11 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    fetch('/sidebar.html')
+        .then(response => {
+            if (!response.ok) throw new Error('Sidebar file not found');
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('sidebar-placeholder').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading sidebar:', error));
+});
---
+++ js/jsTest.js
@@ -0,0 +1,9 @@
+﻿let button = document.querySelector(".random-word-button");
+
+const words = ["Zachary", "Stepp"];
+
+button.addEventListener('click', () => {
+    let num = Math.floor(Math.random() * words.length);
+    button.innerHTML = words[num];
+    button.style.backgroundColor = "random";
+});
